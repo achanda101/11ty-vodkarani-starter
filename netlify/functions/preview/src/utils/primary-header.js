24f54e59c1abb0ddc6841e7fcdf34ae12bf6aca6
Amrita Chanda <amrita.chanda@gmail.com>
2023-09-04 12:02:55 +0530

    Done: Logo animation on navbar scroll using intersection observer

--- netlify/functions/preview/src/utils/primary-header.js
+++ netlify/functions/preview/src/utils/primary-header.js
@@ -1,11 +1,13 @@
 const primaryHeader = document.querySelector('.primary-header')
+const logo = document.querySelector('.logo')
 const scrollWatcher = document.createElement('div')
 
 scrollWatcher.setAttribute('data-scroll-watcher', '')
 primaryHeader.before(scrollWatcher)
 
 const navObserver = new IntersectionObserver((entries) => {
-    primaryHeader.classList.toggle('sticking', !entries[0].isIntersecting)
+    primaryHeader.classList.toggle('sticking', !entries[ 0 ].isIntersecting)
+    logo.classList.toggle('sticking', !entries[0].isIntersecting)
 }, {rootMargin: "100px 0px 0px 0px"})
 
 navObserver.observe(scrollWatcher)
