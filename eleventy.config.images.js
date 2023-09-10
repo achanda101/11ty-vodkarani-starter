d29dd06d9576d2538f56a020697692f928724cc2
Amrita Chanda <amrita.chanda@gmail.com>
2023-09-10 12:11:33 +0530

    Done: {% image %} and {% pictures %} 11ty shortcode to generate <picture> element of webp formats.

--- eleventy.config.images.js
+++ eleventy.config.images.js
@@ -10,8 +10,15 @@ module.exports = eleventyConfig => {
 	}
 
 	// Eleventy Image shortcode
-	// https://www.11ty.dev/docs/plugins/image/
-	eleventyConfig.addAsyncShortcode("image", async function (url, alt, widths, sizes = "(max-width: 800px) 200px, 50vw") {
+	// EXAMPLE
+	// {% image "https://res.cloudinary.com/nrityagram/image/upload/v1643804553/community_card_qpjn0w.jpg",
+    //   "Children's Dance Class",
+    //   ["425", "768", "1600"], "eager" %}
+
+	eleventyConfig.addAsyncShortcode("image", async function (url, alt, widths, sizes = "(max-width: 800px) 200px, 50vw", loadingOption = "lazy") {
+
+		// loadingOption = lazy / eager
+
 		// Full list of formats here: https://www.11ty.dev/docs/plugins/image/#output-formats
 		// Warning: Avif can be resource-intensive so take care!
 		let formats = ["webp", "auto"];
@@ -28,7 +35,7 @@ module.exports = eleventyConfig => {
 		let imageAttributes = {
 			alt,
 			sizes,
-			loading: "lazy",
+			loading: loadingOption,
 			decoding: "async",
 		};
 		return eleventyImage.generateHTML(metadata, imageAttributes);
--- netlify/functions/preview/eleventy.config.pictures.js
+++ netlify/functions/preview/eleventy.config.pictures.js
@@ -9,49 +9,60 @@ module.exports = (eleventyConfig) => {
 		return path.resolve(split.join(path.sep), relativeFilePath);
 	}
 
-	// Eleventy Image shortcode
-	// https://www.11ty.dev/docs/plugins/image/
+	// Eleventy Pictures shortcode
+	
+	// EXAMPLE
+	// {% pictures 
+    //   {"desk":"https://res.cloudinary.com/nrityagram/image/upload/v1647179355/Space-cat2-desk_rix0hl.jpg", 
+    //   "tab":"https://res.cloudinary.com/nrityagram/image/upload/v1620417697/ipad/08-dhruva-ipad_ppl3ra.jpg", 
+    //   "mob":"https://res.cloudinary.com/nrityagram/image/upload/v1582614660/mobile/9_s1ma2d.jpg"}, 
+    //   "Pictures Shortcode Test", 
+    //   {"mob": ["425"], "tab": ["768"], "desk":["1600"]}, {"desk":"65em", "tab":"50em", "mob":"35em"}, 
+    //   "eager", "high" %}
+
 	eleventyConfig.addAsyncShortcode(
 		"pictures",
-		async function (srclist, alt = "Alt Text Req", sizes, screenwidths, fetchOption) {
-			// Full list of formats here: https://www.11ty.dev/docs/plugins/image/#output-formats
-			// Warning: Avif can be resource-intensive so take care!
-			let formats = ["avif", "webp", "auto"];
-			// object of images for different screensizes desk, tab, mob - srclist
+		async function (srclist, alt = "Alt Text Req", widthslist, screenwidths, loadingOption, fetchPriorityOption) {
 			
-			for (const key in srclist) {
-				srclist[key] = relativeToInputPath(this.page.inputPath, srclist[key]);
-			}
+			// fetchOption = low / high
+			// loadingOption = lazy / eager
 
-			let metadatalist = srclist;
-			let metadataKeys = Object.keys(srclist)
+			// Warning: Avif can be resource-intensive so take care!
+			let formats = ["webp", "auto"];
+			
+			// If you are loading images from local folder
+			// for (const key in srclist) {
+			// 	srclist[key] = relativeToInputPath(this.page.inputPath, srclist[key]);
+			// }
 
-			for (const key in srclist) {
+			let metadatalist = {};
+			let metadataKeys = [ "desk", "tab", "mob" ];
+
+			for (const key of metadataKeys) {
 				metadatalist[key] = await eleventyImage(srclist[key], {
-					widths: sizes[key],
+					widths: widthslist[key] || ["auto"],
 					formats,
 					outputDir: path.join(eleventyConfig.dir.output, "img"),
 				});
 			}
-			// generate 9 source tags
+			
+			// generate 6 source tags
 			// TODO: rewrite forEach loop using map() and join()
 			let sourceList = ''
 			metadataKeys.forEach(key => {
 				let formats = Object.keys(metadatalist[ key ])
 				formats.forEach(format => {
 					let formatDetails = metadatalist[key][format]
-					let source = `<source srcset="${formatDetails[ 0 ].url}" width="${formatDetails[0].width}" height="${formatDetails[0].height}" media="(min-width: ${screenwidths[ key ]}px)" type="image/${formatDetails[ 0 ].format}" />`
+					let source = `<source srcset="${formatDetails[ 0 ].url}" width="${formatDetails[0].width}" height="${formatDetails[0].height}" media="(min-width: ${screenwidths[ key ]})" type="image/${formatDetails[ 0 ].format}" />`
 					sourceList = sourceList.concat(source)
 				})
 			})
 			
 			// create img string with webp from mobile
 			let imgData = metadatalist[ metadataKeys.slice(-1) ][formats[0]][0]
-			let imgElem = `<img src="${imgData.url}" width="${imgData.width}" height="${imgData.height}" alt="${alt}" fetchPriority="${fetchOption}" />`
+			let imgElem = `<img src="${imgData.url}" width="${imgData.width}" height="${imgData.height}" alt="${alt}" fetchPriority="${fetchPriorityOption}" loading="${loadingOption}" />`
 
 			return `<picture>${sourceList}${imgElem}</picture>`
-
-			// TODO loading=eager
 		}
 	);
 };
